0a0653c1378b459d3a2a231f4af33b65aa140eab
Anoop Kumar Narayanan <anoop.kn@live.in>
2024-01-15 21:53:37 +0530

    VIEW CHANGELOG 15/01/2024 - 2

--- log.cpp
+++ log.cpp
@@ -1,3 +1,4 @@
+//Copyright Anoop Kumar Narayanan <anoop.kumar.narayanan@gmail.com> , LICENSE - GPLv2 / GPLv3
 #include <stdio.h>
 #include <log.h>
 #include <stdarg.h>
--- log.h
+++ log.h
@@ -1,3 +1,4 @@
+//Copyright Anoop Kumar Narayanan <anoop.kumar.narayanan@gmail.com> , LICENSE - GPLv2 / GPLv3
 #ifndef HTTPD_LOG_H
 #define HTTPD_LOG_H
 
