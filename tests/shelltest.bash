28194dc506c2cef89efc6ee92e5039ab7fefa285
Anoop Kumar Narayanan <anoop.kn@live.in>
2024-11-20 22:29:07 +0530

    Add files via upload

--- tests/shelltest.bash
+++ tests/shelltest.bash
@@ -9,6 +9,7 @@ curl -vvv -H "Term-Id: $termid" --data-urlencode "command=EXECUTE" --data-urlenc
 curl -vvv -H "Term-Id: $termid" --data-urlencode "command=EXECUTE" --data-urlencode 'op=ps -ef| grep httpdsrv' http://localhost:15000/bash.xyz
 curl -vvv -H "Term-Id: $termid" --data-urlencode "command=EXECUTE" --data-urlencode 'op=echo alldone' http://localhost:15000/bash.xyz
 curl -vvv -H "Term-Id: $termid" --data-urlencode "command=EXECUTE" --data-urlencode 'op=i=1; while [ $i -lt 11 ]; do echo "whiling $i"; i=$((i+1)); done' http://localhost:15000/bash.xyz
+curl -vvv -H "Term-Id: $termid" -H "Content-type', 'application/x-www-form-urlencoded; charset=utf-8" --data-urlencode "command=EXECUTE" --data-urlencode 'op=i=1; while [ $i -lt 11 ]; do echo "whiling $i"; i=$((i+1)); done' http://localhost:15000/bash.xyz
 curl -vvv -H "Term-Id: $termid" --data-urlencode "command=STOP" http://localhost:15000/bash.xyz
 
 echo $termid
