73679c50feaff896a7f13865f71952d5cc886a72
Anoop Kumar Narayanan <anoop.kn@live.in>
2025-05-30 00:11:09 +0530

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl req -new -x509 -newkey rsa:2048 -keyout httpdkey.pem -out httpdcert.pem -days 3650
+openssl req -new -nodes -x509 -newkey rsa:2048 -keyout httpdkey.pem -out httpdcert.pem -days 3650
